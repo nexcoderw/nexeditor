310aa6c52d6aa0ae80e92247e67ee46e451ae961
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-07 15:27:14 +0200

    chore(ui): add Toolbar barrel export for clean public imports

---
+++ packages/core/src/ui/Toolbar/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Toolbar barrel export.
+ * Consumers who want to use toolbar components independently can import from here.
+ */
+export { Toolbar } from './Toolbar';
+export { ToolbarButton } from './ToolbarButton';
+export { ToolbarIcon } from './ToolbarIcon';
+export type { ToolbarProps } from './Toolbar';
+export type { ToolbarButtonProps } from './ToolbarButton';
